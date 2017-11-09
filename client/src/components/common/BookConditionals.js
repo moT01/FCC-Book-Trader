3a1773872ad397275a9c0d7cecbc6ff1a4422d7f
moT01 <TMondloch01@gmail.com>
2017-11-09 11:21:30 -0600

    change requested_From variables to match

--- client/src/components/common/BookConditionals.js
+++ client/src/components/common/BookConditionals.js
@@ -6,15 +6,18 @@ export function GetBookStatus(props) {
     return <div></div>
   }
 
-  //if my book
+  //if my book  
   if(props.username === props.book.current_owner) {
     return <button onClick={props.deleteBook} className="btn btn-danger bookButtonRight">delete</button>
   }
   
-  //more cases = if user has already requested -> unrequest button
-  //if book.requestedFrom[]
-  //return <button onClick={props.unrequestBook} className="btn btn-secondary bookButtonRight">unrequest</button>
-
+  //if I already requested
+  for(let i=0; i<props.book.requested_From.length; i++) {
+    if (props.book.requested_From[i] === props.username) {
+      return <button onClick={props.unrequestBook} className="btn btn-secondary bookButtonRight">unrequest</button>
+    }
+  }
+  
   //if not my book - and not already requested by me
   if(props.username !== props.book.current_owner) {
     return <button onClick={props.requestBook} className="btn btn-primary bookButtonRight">request</button>
